9fd9cd824fe8fde9864c246f81c063a7031388b6
Lektonic <lektonic@lektonic.com>
2024-06-05 15:59:03 -0500

    Added i3 configuration

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ if [ -z "$XDG_CONFIG_HOME" ]; then XDG_CONFIG_HOME="$HOME/.config"; fi
 ln -s ${PWD}/nvim ${XDG_CONFIG_HOME}/nvim
 ln -s ${PWD}/tmux ${XDG_CONFIG_HOME}/tmux
 ln -s ${PWD}/alacritty ${XDG_CONFIG_HOME}/alacritty
+ln -s ${PWD}/i3 ${XDG_CONFIG_HOME}/i3
